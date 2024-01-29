6b75d715b52c3f670184808eb555289c815b16cc
Wojciech Batorski <wojbat4@gmail.com>
2024-01-29 22:14:18 +0100

    Add Trip domain

---
+++ trip/models/domain/trip.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from device import Device
+from core import Geometry
+
+__all__ = ("Trip",)
+
+
+@dataclass(frozen=True)
+class Trip:
+    id: UUID
+    geometry: Geometry
+    device: Device
+    started_at: datetime
+    finished_at: Optional[datetime] = None
