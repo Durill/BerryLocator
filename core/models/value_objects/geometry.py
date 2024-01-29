f9d1e52f026d116e716ee423ef58022d2aee96d9
Wojciech Batorski <wojbat4@gmail.com>
2024-01-29 20:07:07 +0100

    Implement Geometry class

--- core/models/value_objects/geometry.py
+++ core/models/value_objects/geometry.py
@@ -1,7 +1,53 @@
+from __future__ import annotations
 from typing import Union
 
+from enum import IntEnum
+
+from shapely.errors import ShapelyError
 from shapely.geometry.base import BaseGeometry, BaseMultipartGeometry
+from shapely.geometry import shape as as_shape
+
+__all__ = (
+    "SRID_EPSG",
+    "Geometry",
+)
+
+
+class SRID_EPSG(IntEnum):
+    _3857 = 3857
+    _4326 = 4326
+
+    @classmethod
+    def as_expression(cls, enum: SRID_EPSG) -> str:
+        return f"EPSG:{enum.value}"
 
 
 class Geometry:
+    """
+    Value of geometry is parsed from valid GeoJSON
+
+    For full format reference:
+    https://www.rfc-editor.org/rfc/rfc7946#appendix-A
+
+    For transformation check see:
+    https://epsg.io/
+    """
     value: Union[BaseGeometry, BaseMultipartGeometry]
+    srid: SRID_EPSG = SRID_EPSG._4326  # WGS-84 - default for GeoJSON
+
+    @classmethod
+    def create(
+        cls,
+        geojson: dict,
+        geojson_srid: SRID_EPSG
+    ) -> Geometry:
+
+        if not isinstance(geojson, dict):
+            raise  # Geometry invalid argument
+
+        try:
+            shape = as_shape(context=geojson)
+        except (TypeError, ValueError) as error:
+            raise  # Geometry not parsable
+        except ShapelyError as error:
+            raise  # Geometry not parsable from e
