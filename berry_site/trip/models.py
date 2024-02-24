0b7d864fdeb7162eaf182733266cf7c7fd67d65b
Wojciech Batorski <wojbat4@gmail.com>
2024-02-24 12:18:24 +0100

    Fix trip model to handle geometry

--- berry_site/trip/models.py
+++ berry_site/trip/models.py
@@ -1,10 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 from berry_site.device.models import DeviceModel
 
 
 class TripModel(models.Model):
-    geometry = models.TextField()
+    geometry = models.GeometryField(srid=4326)
     device_id = models.ForeignKey(DeviceModel, on_delete=models.CASCADE)
     started_at = models.DateTimeField()
     finished_at = models.DateTimeField(null=True)
